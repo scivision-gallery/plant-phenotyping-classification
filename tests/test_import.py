bd38f721ac86ebab30be0a8b17c46f79890625bb
Kasra Hosseini <khosseini@turing.ac.uk>
2022-03-16 19:31:19 +0000

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -9,6 +9,7 @@ def test_workflow():
 
     cat = load_dataset('https://github.com/alan-turing-institute/mapreader-plant-scivision')
 
+    # --- plant_single dataset
     ds = cat.plant_single().to_dask()
 
     outputs = model.predict(ds[0], plot_output=False)
@@ -16,3 +17,10 @@ def test_workflow():
     
     outputs = model.predict(ds[1], plot_output=False)
     assert len(outputs) == 30
+    
+    # --- plant_flower dataset 
+    ds = cat.plant_flower().to_dask()
+
+    outputs = model.predict(ds[0], plot_output=False)
+    
+    outputs = model.predict(ds[1], plot_output=False)
