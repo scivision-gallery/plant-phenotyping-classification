40c91dd68c32134eadb511a74aa325b7219c4b0f
Evangeline Corcoran <82043547+evangeline-corcoran@users.noreply.github.com>
2022-03-18 17:44:09 +0000

    Update test_import.py
    update test_import.py to match new version of mapreader-plant-scivision.ipynb based on scivision-core-functionality.ipynb

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,26 +1,25 @@
 import pytest
 
 def test_workflow():
-    from scivision.io import load_pretrained_model, load_dataset
+    from scivision import default_catalog, load_pretrained_model, load_dataset
 
-    scivision_yml = 'https://raw.githubusercontent.com/alan-turing-institute/mapreader-plant-scivision/main/.scivision-config.yaml'
+    models_catalog = default_catalog.models.to_dataframe()
+    stp_repo = models_catalog[models_catalog.name == "mapreader-plant"].url.item()
     
-    model = load_pretrained_model(scivision_yml, allow_install=True)
-
-    cat = load_dataset('https://github.com/alan-turing-institute/mapreader-plant-scivision/blob/main/scivision.yml')
-
-    # --- plant_single dataset
-    ds = cat.plant_single().to_dask()
-
-    outputs = model.predict(ds[0], plot_output=False)
-    assert len(outputs) == 30
+    model = load_pretrained_model(stp_repo, allow_install=True)
     
-    outputs = model.predict(ds[1], plot_output=False)
-    assert len(outputs) == 30
+    data_config = load_dataset('https://github.com/alan-turing-institute/mapreader-plant-scivision')
     
+    # --- plant_single dataset 
+    plant_single = data_config.plant_single().to_dask()
+
+    model.predict(plant_single[0], slice_size=25)
+    
+    model.predict(plant_single[1], slice_size=25))
+
     # --- plant_flower dataset 
-    ds = cat.plant_flower().to_dask()
+    plant_flower = data_config.plant_flower().to_dask()
 
-    outputs = model.predict(ds[0], plot_output=False)
+    model.predict(plant_flower[0], slice_size=25)
     
-    outputs = model.predict(ds[1], plot_output=False)
+    model.predict(plant_flower[1], slice_size=25))
